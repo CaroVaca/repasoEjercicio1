91d90923d46ff89b30f3b0be706daf272c9b39c8
CaroVaca <104107454+CaroVaca@users.noreply.github.com>
2022-06-03 16:08:55 -0300

    agregue funciones agregue las funciones de menorNota, mayorNota y calcularPromedio

--- src/index.ts
+++ src/index.ts
@@ -15,10 +15,13 @@ let meses: string[] = [
 ];
 let mesMin: string;
 let minimaNota: number = 11;
+let mesMax: string;
+let maximaNota: number = -1;
+let promedio: number;
+let indice: number;
 
 function leerMesesDesaprobados(notasAlumno: number[], meses: string[]): string {
   let mesesDesaprobados: string = " ";
-  let indice: number;
   for (indice = 0; indice < notasAlumno.length; indice++) {
     if (notasAlumno[indice] < 6) {
       mesesDesaprobados += meses[indice] + ",";
@@ -28,7 +31,6 @@ function leerMesesDesaprobados(notasAlumno: number[], meses: string[]): string {
 }
 
 function menorNota(notasAlumno: number[], meses: string[]) {
-  let indice: number;
   for (indice = 0; indice < notasAlumno.length; indice++) {
     if (notasAlumno[indice] < minimaNota) {
       mesMin = meses[indice];
@@ -37,5 +39,28 @@ function menorNota(notasAlumno: number[], meses: string[]) {
   }
   return "El mes que mas baja nota tuvo es: " + mesMin + " con: " + minimaNota;
 }
+function mayorNota(notasAlumno: number[], meses: string[]) {
+  for (indice = 0; indice < notasAlumno.length; indice++) {
+    if (notasAlumno[indice] > maximaNota) {
+      mesMax = meses[indice];
+      maximaNota = notasAlumno[indice];
+    }
+  }
+  return "El mes que mas alta nota tuvo es: " + mesMax + " con: " + maximaNota;
+}
+function calcularPromedio(notasAlumno: number[], meses: string[]) {
+  let sumaTotal: number = 0;
+  for (indice = 0; indice < notasAlumno.length; indice++) {
+    sumaTotal += notasAlumno[indice];
+  }
+  promedio = sumaTotal / 10;
+  return promedio;
+}
+console.log("Meses que desaprobo el Alumno");
 console.log(leerMesesDesaprobados(notasAlumno, meses));
+console.log("Mes en que obtuvo la menor nota");
 console.log(menorNota(notasAlumno, meses));
+console.log("Mes en el que obtuvo la mayor nota");
+console.log(mayorNota(notasAlumno, meses));
+console.log("Promedio del Alumno");
+console.log(calcularPromedio(notasAlumno, meses));
